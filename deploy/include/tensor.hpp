e11e5c6fe2165221052ab60cfe1b363a925e8b9b
ValKmjolnir <lhk101lhk101@qq.com>
2026-08-26 01:11:47 +0800

    :bug: fix operator[] for tensor
    Signed-off-by: ValKmjolnir <lhk101lhk101@qq.com>

--- deploy/include/tensor.hpp
+++ deploy/include/tensor.hpp
@@ -15,68 +15,71 @@
 #include <type_traits>
 #include <vector>
 
+#include <cassert>
+
 template<typename T>
 class tensor_view {
 private:
-    std::vector<std::size_t> shape_;
-    std::vector<std::size_t> strides_;
-    const T* data_;
+    const std::vector<std::size_t>* shape_;
+    const std::vector<std::size_t>* strides_;
+    T* data_;
+    std::size_t dim_;
+    std::size_t offset_;
 
 public:
-    tensor_view(const std::vector<std::size_t>& shape, const std::vector<std::size_t>& strides, const T* data) {
-        shape_ = shape;
-        strides_ = strides;
-        data_ = data;
-    }
+    tensor_view(const std::vector<std::size_t>& shape,
+                const std::vector<std::size_t>& strides,
+                T* data,
+                std::size_t dim,
+                std::size_t offset):
+                shape_(&shape), strides_(&strides),
+                data_(data), dim_(dim), offset_(offset) {}
 
     tensor_view operator[](std::size_t i) const {
-        if (i >= shape_[0]) {
-            throw std::out_of_range("index out of range");
-        }
-        const T* offset = data_ + strides_[0] * i;
-        std::vector<std::size_t> shape = std::vector<std::size_t>(shape_.begin() + 1, shape_.end());
-        std::vector<std::size_t> strides = std::vector<std::size_t>(strides_.begin() + 1, strides_.end());
-        return tensor_view<T>(shape, strides, offset);
+        assert(dim_ < shape_->size());
+
+        std::size_t offset = offset_ + (*strides_)[dim_] * i;
+        return tensor_view<T>(*shape_, *strides_, data_, dim_ + 1, offset);
     }
 
     operator T&() {
-        return data_[0];
+        assert(dim_ == shape_->size());
+        return data_[offset_];
     }
 
     operator const T&() const {
-        return data_[0];
+        assert(dim_ == shape_->size());
+        return data_[offset_];
     }
 
-    void dump(std::ostream& out, size_t indent = 0) const {
-        if (shape_.size() == 1) {
-            for (std::size_t i = 0; i < indent; i++) {
-                out << " ";
+    void dump(std::ostream& out, size_t indent = 0) {
+        if (dim_ == shape_->size()) {
+            out << data_[offset_];
+            return;
+        }
+
+        for (std::size_t i = 0; i < indent; i++) {
+            out << " ";
+        }
+        out << "[";
+        if (shape_->size() > 1 && dim_ < shape_->size() - 1) {
+            out << "\n";
+        }
+        for (std::size_t i = 0; i < (*shape_)[dim_]; i++) {
+            (*this)[i].dump(out, indent + 1);
+            if (i != (*shape_)[dim_] - 1) {
+                out << ",";
             }
-            out << "[";
-            for (std::size_t i = 0; i < shape_[0]; i++) {
-                out << data_[i];
-                if (i != shape_[0] - 1) {
-                    out << ", ";
-                }
-            }
-            out << "]";
-        } else {
-            for (std::size_t i = 0; i < indent; i++) {
-                out << " ";
-            }
-            out << "[\n";
-            for (std::size_t i = 0; i < shape_[0]; i++) {
-                (*this)[i].dump(out, indent + 1);
-                if (i != shape_[0] - 1) {
-                    out << ",";
-                }
+            if (shape_->size() > 1 && dim_ < shape_->size() - 1) {
                 out << "\n";
             }
+        }
+        if (shape_->size() > 1 && dim_ < shape_->size() - 1) {
             for (std::size_t i = 0; i < indent; i++) {
                 out << " ";
             }
-            out << "]";
         }
+        out << "]";
     }
 };
 
@@ -88,34 +91,48 @@ private:
     std::vector<std::size_t> strides_;
     std::vector<T> data_;
 
+private:
+    std::size_t total_size() {
+        std::size_t n = 1;
+        for (auto& i : shape_) {
+            n *= i;
+        }
+        return n;
+    }
+
+    void compute_strides() {
+        strides_.resize(shape_.size());
+        std::size_t stride = 1;
+        for (int i = static_cast<int>(shape_.size()) - 1; i >= 0; i--) {
+            strides_[i] = stride;
+            stride *= shape_[i];
+        }
+    }
+
 public:
-    tensor(const std::vector<std::size_t>& shape) {
-        shape_ = shape;
-        strides_ = std::vector<std::size_t>(shape.size());
-        strides_[shape.size() - 1] = 1;
-        for (std::size_t i = shape.size() - 2; ; i--) {
-            strides_[i] = strides_[i + 1] * shape[i + 1];
-            if (i == 0) {
-                break;
-            }
-        }
-
-        data_ = std::vector<T>(strides_[0] * shape[0]);
+    tensor(const std::vector<std::size_t>& shape):
+        shape_(shape), data_(total_size()) {
+        compute_strides();
     }
 
-    tensor_view<T> operator[](std::size_t i) const {
-        if (i >= shape_[0]) {
-            throw std::out_of_range("index out of range");
-        }
-        const T* offset = data_.data() + strides_[0] * i;
-        std::vector<std::size_t> shape = std::vector<std::size_t>(shape_.begin() + 1, shape_.end());
-        std::vector<std::size_t> strides = std::vector<std::size_t>(strides_.begin() + 1, strides_.end());
-        return tensor_view<T>(shape, strides, offset);
+    tensor(std::initializer_list<std::size_t> shape):
+        shape_(shape), data_(total_size()) {
+        compute_strides();
     }
 
-    std::vector<T>& data() {
-        return data_;
+    tensor_view<T> operator[](std::size_t i) {
+        assert(!shape_.empty());
+
+        std::size_t offset = strides_[0] * i;
+        return tensor_view<T>(shape_, strides_, data_.data(), 1, offset);
     }
+
+    std::vector<T>& vec() { return data_; }
+    const std::vector<T>& vec() const { return data_; }
+
+    T* data() { return data_.data(); }
+
+    const T* data() const { return data_.data(); }
 
     tensor& transpose(std::size_t i, std::size_t j) {
         std::swap(shape_[i], shape_[j]);
@@ -123,7 +140,7 @@ public:
         return *this;
     }
 
-    void dump(std::ostream& out, size_t indent = 0) const {
+    void dump(std::ostream& out, size_t indent = 0) {
         out << "shape: ";
         for (auto& i : shape_) {
             out << i << " ";
@@ -135,6 +152,18 @@ public:
             out << i << " ";
         }
         out << std::endl;
+
+        if (shape_.size() == 1) {
+            out << "[";
+            for (std::size_t i = 0; i < shape_[0]; i++) {
+                out << data_[i];
+                if (i != shape_[0] - 1) {
+                    out << ", ";
+                }
+            }
+            out << "]" << std::endl;
+            return;
+        }
 
         for (std::size_t i = 0; i < indent; i++) {
             out << " ";
--- deploy/tensor_test.cpp
+++ deploy/tensor_test.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 #include "include/tensor.hpp"
+
+void test_vector() {
+    std::vector<std::size_t> shape = {2};
+    tensor<float> a(shape);
+    a.vec() = {1, 2};
+    a.dump(std::cout);
+
+    std::cout << "[" << a[0] << ", " << a[1] << "]" << std::endl;
+}
 
 void test() {
     tensor<float> a({2, 3, 4});
-    a.data() = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    a.vec() = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
     try {
         a.dump(std::cout);
         a.transpose(0, 1);
@@ -13,7 +23,7 @@ void test() {
     }
 
     tensor<float> b({2, 3, 4});
-    b.data() = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    b.vec() = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
     try {
         b.dump(std::cout);
         b.transpose(0, 2);
@@ -24,6 +34,7 @@ void test() {
 }
 
 int main() {
+    test_vector();
     test();
     return 0;
 }
